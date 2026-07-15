137b66c5a1982a68fd555c7ef53f89d93e138d3c
Ishan Mishra <ishanmishra128@gmail.com>
2026-07-15 15:22:37 +0000

    Clean up & remove duplicate cols

--- models/marts/core/fct_sessions.sql
+++ models/marts/core/fct_sessions.sql
@@ -1,41 +1,29 @@
-{{ config(materialized='view') }}
-
-with sessions as (
-
-    select *
-    from {{ ref('int_sessions') }}
-
-)
+{{ config(
+    materialized = 'table'
+) }}
 
 select
     session_key,
     user_pseudo_id,
     ga_session_id,
-
     session_date,
     session_start_ts,
     session_end_ts,
     session_duration_seconds,
-
     event_count,
     distinct_event_count,
     session_start_count,
-
     page_view_count,
     product_view_count,
     add_to_cart_count,
     begin_checkout_count,
     purchase_count,
     transaction_count,
-
     has_purchase,
     has_transaction,
-
     first_event_name,
     last_event_name,
-
     platform,
     device_category,
     country
-
-from sessions
+from {{ ref('int_sessions') }}
